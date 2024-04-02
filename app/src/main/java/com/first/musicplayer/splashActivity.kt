436442430e528295d40bb7e5c37d127a9b3e6737
Ashu-void <ashudking64@gmail.com>
2024-04-02 23:12:23 +0530

    Music App

---
+++ app/src/main/java/com/first/musicplayer/splashActivity.kt
@@ -0,0 +1,21 @@
+package com.first.musicplayer
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        setContentView(R.layout.splash_activity)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },6000)
+    }
+}
